1905eca0d5e9f6b58190b2fa7d7088701bb07d45
bhuvankachhadiya <137286464+bhuvankachhadiya@users.noreply.github.com>
2023-06-29 16:48:10 +0530

    Add files via upload

---
+++ EVENODD.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char n;
+clrscr();
+printf("please enter any natuarl number=");
+scanf("%c",&n);
+(n%2==0)?printf("number is even"):printf("number is odd");
+getch();
+}
